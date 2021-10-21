04c8ceddbbde673ae838e0089460d6825bdd4cbb
stef <sc.shher@gmail.com>
2021-10-20 22:42:49 -0400

    Finished Task B

--- funcs.cpp
+++ funcs.cpp
@@ -12,12 +12,27 @@ std::string removeLeadingSpaces(std::string line)
   for (int i = 0; i < line.length(); i++)
   {
     char curr = line[i];
-    
+
     if (!isspace(curr))
       start = true;
 
     if (start)
       result += curr;
+  }
+
+  return result;
+}
+
+//----------------- task B -----------------
+
+int countChar(std::string line, char c)
+{
+  int result = 0;
+
+  for (int i = 0; i < line.length(); i++)
+  {
+    if (line[i] == c)
+      result++;
   }
 
   return result;
